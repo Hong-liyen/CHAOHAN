b1d02407e3b00f3dd30ac2500a115865f6b06b62
Amanda <amanda770328@gmail.com>
2025-05-10 10:37:55 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// 手機板選單
+const nav_bar = document.querySelector('.nav_bar_left');
+const header_menu = document.querySelector('.header_menu');
+const bar = document.querySelector('.bar');
+const delete_menu = document.querySelector('.delete');
+
+
+function barMenu() {
+    header_menu.classList.toggle('open')
+    if(header_menu.classList.contains('open')) {
+        bar.style.display = 'none'
+        delete_menu.style.display = 'block'
+    } else {
+        delete_menu.style.display = 'none'
+        bar.style.display = 'block'
+    }
+}
+
+// 手機板登入下拉選單
+const dropdown_menu = document.querySelector('.dropdown_menu');
+const dropdown_arrow = document.querySelector('.dropdown_arrow');
+
+
+function login() {
+    dropdown_menu.classList.toggle('open')
+    if(!dropdown_menu.classList.contains('open')) {
+        dropdown_arrow.style.transform = 'rotate(180deg)'
+    } else {
+        dropdown_arrow.style.transform = 'rotate(0deg)'
+    }
+}
+
+// QA常見問題下拉選單
+const answer_text = document.getElementsByClassName('answer');
+
+for(i=0; i<answer_text.length; i++) {
+    answer_text[i].addEventListener('click', function(){
+        this.classList.toggle('auto')
+    })
+}
